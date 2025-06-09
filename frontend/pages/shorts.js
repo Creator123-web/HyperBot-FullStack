d002ad065c1bbb5dfa7e49ccb81ad3e92237479d
Creator123-web <um5233031@gmail.com>
2025-06-09 23:00:53 +0530

    Create shorts.js

---
+++ frontend/pages/shorts.js
@@ -0,0 +1,10 @@
+import ShortsEditor from '@/components/ShortsEditor';
+
+export default function ShortsPage() {
+  return (
+    <div>
+      <h1>Shorts Editor</h1>
+      <ShortsEditor />
+    </div>
+  );
+}
