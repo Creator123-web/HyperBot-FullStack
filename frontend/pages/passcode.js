269ac65a175a69a1cd3581dbb0370f90ed5c385b
Creator123-web <um5233031@gmail.com>
2025-06-09 23:00:01 +0530

    Create passcode.js

---
+++ frontend/pages/passcode.js
@@ -0,0 +1,10 @@
+import PasscodeProtection from '@/components/PasscodeProtection';
+
+export default function PasscodePage() {
+  return (
+    <div>
+      <h1>Passcode Protection Page</h1>
+      <PasscodeProtection />
+    </div>
+  );
+}
