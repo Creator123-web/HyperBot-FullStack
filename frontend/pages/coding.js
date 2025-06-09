90e50d3c6527804588804ae30aa57b5d764c95cc
Creator123-web <um5233031@gmail.com>
2025-06-09 22:58:43 +0530

    Create coding.js

---
+++ frontend/pages/coding.js
@@ -0,0 +1,10 @@
+import CodingPractice from '@/components/CodingPractice';
+
+export default function CodingPage() {
+  return (
+    <div>
+      <h1>Coding Practice</h1>
+      <CodingPractice />
+    </div>
+  );
+}
