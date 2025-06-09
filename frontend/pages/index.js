7c5754d9b927fb47536e7ca49d451bef7367ed59
Creator123-web <um5233031@gmail.com>
2025-06-09 22:57:20 +0530

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import PasscodeProtection from '@/components/PasscodeProtection';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>HyperBot AI Assistant</h1>
+      <PasscodeProtection />
+    </div>
+  );
+}
